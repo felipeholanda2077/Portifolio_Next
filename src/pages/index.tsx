ba44509150254b92437639f271ce787a4c5e3be6
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2022-08-16 11:24:53 -0300

    Criação da Nova Versão do projeto Home Page

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import { HomeContainer } from '../styles/HomeStyles';
+import Header from '../components/Header';
+import { HomeHero } from '../components/HomeHero';
+import Experiencias from '../components/Experiencias';
+import Projetos from '../components/Projetos';
+import Conhecimentos from '../components/Conhecimentos';
+import Footer from '../components/Footer';
+import FormContato from '../components/FormContato';
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <Header />
+
+      <main className="container">
+        <HomeHero />
+        <Experiencias />
+        <Projetos />
+        <Conhecimentos />
+        <FormContato />
+      </main>
+      <Footer />
+    </HomeContainer>
+  );
+}
