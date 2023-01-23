daab05402b27e248cbb64916f673ea94726fda82
Felipe Holanda <79938668+felipeholanda2077@users.noreply.github.com>
2023-01-23 19:51:25 -0300

    Update Form.tsx

--- src/components/FormContato/Form.tsx
+++ src/components/FormContato/Form.tsx
@@ -8,7 +8,7 @@ export default function Form() {
 
   return (
     <FormContainer>
-      <form>
+      <form action="mailto:teste@mail.com" method="get" id="contatoForm" target="_blank">
         <Input
           placeholder="Nome"
           value={nome}
@@ -29,7 +29,16 @@ export default function Form() {
           onChange={({ target }) => setMensagem(target.value)}
         />
       </form>
-      <button type="button">ENVIAR</button>
+      <button type="button" id="enviar">ENVIAR</button>
     </FormContainer>
   );
 }
+
+<script>
+  document.getElementById('contatoForm').addEventListener('submit', function () {
+    var nome = this.querySelector('input[name=nome]'), nome = nome.value;
+    var email = this.querySelector('input[name=email]'), email = email.value;
+    var texto = 'Olá destinatário, \nMeu nome é '+ nome +' e meu email é '+ email;
+    this.querySelector('input[name=Body]').setAttribute('value', texto);
+});
+  </script>
