b95bc51343c7a244d9313003a74fac1244bd8171
Felipe Holanda <79938668+felipeholanda2077@users.noreply.github.com>
2023-01-23 19:54:05 -0300

    Update Form.tsx

--- src/components/FormContato/Form.tsx
+++ src/components/FormContato/Form.tsx
@@ -33,12 +33,3 @@ export default function Form() {
     </FormContainer>
   );
 }
-
-<script>
-  document.getElementById('contatoForm').addEventListener('submit', function () {
-    var nome = this.querySelector('input[name=nome]'), nome = nome.value;
-    var email = this.querySelector('input[name=email]'), email = email.value;
-    var texto = 'Olá destinatário, \nMeu nome é '+ nome +' e meu email é '+ email;
-    this.querySelector('input[name=Body]').setAttribute('value', texto);
-});
-  </script>
